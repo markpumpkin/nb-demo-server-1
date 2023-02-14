fbac45658457ebbe45e61930c9b8c303399c95cc
quangnb <quangnb@magezon.com>
2023-02-14 15:15:53 +0700

    update

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.static("public"));
 app.get("/", function (req, res) {
     res.send("<h1>Hello World!</h1>");
 });
+app.get("/abc", function (req, res) {
+    res.send("<h1>Hello World!</h1>");
+});
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
